cde6f27f1e227b7c26311b60e3a7c74115c87f5c
nathanspang <nathanspang@tamu.edu>
2021-06-10 20:00:48 -0500

    added specific column type classes

--- classes.py
+++ classes.py
@@ -3,10 +3,22 @@ class DataSheet:
         self.dataframe = dataframe
 
     def get_cols(self):
+        cols = {}
         for col in self.dataframe.columns:
-            print(col)
+            cols[col] = Column(col)
+        return cols
 
 
 class Column:
     def __init__(self, name):
         self.name = name
+
+class CategoricalColumn(Column):
+    def __init__(self, value, binary):
+        self.value = value
+        self.binary = binary
+
+class NumericalColumn(Column):
+    def __init__(self, value, imput_method):
+        self.value = value
+        self.imput_method = imput_method
--- code.py
+++ code.py
@@ -3,6 +3,7 @@
 import pandas as pd
 from classes import DataSheet
 
+print()
 print("--------------- Data Cleansing Automation Script ---------------")
 print()
 print("Welcome to the Data Cleansing Automation Script 1.0")
@@ -17,4 +18,27 @@ else:
 
 orig_data = pd.read_csv(file_name)
 sheet = DataSheet(orig_data)
-sheet.get_cols()
+columns = sheet.get_cols()
+
+kept_cols = []
+categorical_cols = []
+numerical_cols = []
+for col in columns:
+    print()
+    value = input("Will the {0} column be valuable for the analysis? (Y/N) ".format(col))
+    if value.lower() == "y":
+        kept_cols.append(col)
+
+        type = input("Is the {0} column numerical or categorical?: ".format(col))
+        if type.lower() == "categorical":
+            categorical_cols.append(col)
+            print()
+            
+
+        else:
+            numerical_cols.append(col)
+            print()
+
+
+    else:
+        print(f"Column {col} will be removed")
